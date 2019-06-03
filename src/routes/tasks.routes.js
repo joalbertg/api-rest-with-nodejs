7e9ae6c05ac6fc9935f2e3bc11b65e1ad9e31960
Joalbert <joalbertgonzalez@gmail.com>
2019-06-02 20:53:32 -0500

    add: task get by id method route

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { ObjectID } from 'mongodb';
 
 // Database connection
 import connect from '../database';
@@ -17,7 +18,16 @@ router.post('/', async (req, res) => {
   const { title, description } = req.body;
   const task = { title, description }
   const result = await db.collection('task').insertOne(task);
+
   res.json(result.ops[0]);
+});
+
+router.get('/:id', async (req, res) => {
+  const db = await connect();
+  const { id } = req.params;
+  const result = await db.collection('task').findOne({ _id: ObjectID(id) });
+
+  res.json(result);
 });
 
 export default router;
